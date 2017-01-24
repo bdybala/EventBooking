d9b81da283c4e7ac3c10924b981daedd43ab3661
Bartosz Dybała <dybala.bartosz@gmail.com>
2017-01-24 16:24:15 +0100

    change constants to upper letters

--- src/utils/Settings.java
+++ src/utils/Settings.java
@@ -3,9 +3,9 @@ package utils;
 public final class Settings {
 
 	// main window
-	public final static Double mainWindowWidth = 640.;
-	public final static Double mainWindowHeight = 480.;
+	public final static Double MAIN_WINDOW_WIDTH = 640.;
+	public final static Double MAIN_WINDOW_HEIGHT = 480.;
 	// create new event window
-	public final static Double eventFormWidth = 450.;
-	public final static Double eventFormHeight = 450.;
+	public final static Double EVENT_FORM_WIDTH = 450.;
+	public final static Double EVENT_FORM_HEIGHT = 450.;
 }
