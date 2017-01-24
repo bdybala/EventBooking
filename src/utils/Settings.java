ac59585f1a75784bc8819956e7fe8d44cdacf456
Bartosz Dybała <dybala.bartosz@gmail.com>
2017-01-24 16:21:48 +0100

    change constants visibility to public

--- src/utils/Settings.java
+++ src/utils/Settings.java
@@ -3,9 +3,9 @@ package utils;
 public final class Settings {
 
 	// main window
-	final static Double mainWindowWidth = 640.;
-	final static Double mainWindowHeight = 480.;
+	public final static Double mainWindowWidth = 640.;
+	public final static Double mainWindowHeight = 480.;
 	// create new event window
-	final static Double eventFormWidth = 450.;
-	final static Double eventFormHeight = 450.;
+	public final static Double eventFormWidth = 450.;
+	public final static Double eventFormHeight = 450.;
 }
