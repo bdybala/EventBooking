b8fedc325e2434e51607b6fd247268f8c393f2e7
Bartosz Dybała <dybala.bartosz@gmail.com>
2017-01-24 03:05:28 +0100

    init TODO ticketDao methods

---
+++ src/data/ticket/TicketDao.java
@@ -0,0 +1,42 @@
+package data.ticket;
+
+import java.util.List;
+
+import data.dao.DataAccessObject;
+
+public class TicketDao extends DataAccessObject {
+
+	public Ticket getTicketById(int id) {
+		Ticket t = null;
+		// TODO
+		return t;
+	}
+	
+	public List<Ticket> getAllTickets() {
+		List<Ticket> ticketList = null;
+		// TODO
+		return ticketList;
+	}
+	
+	public List<Ticket> getUserTickets(int userid) {
+		List<Ticket> ticketList = null;
+		// TODO
+		return ticketList;
+	}
+	
+	public int insertTicket(Ticket t) {
+		int rows = 0;
+		// TODO
+		return rows;
+	}
+	
+	public int updateTicket() {
+		//TODO
+		return 0;
+	}
+	
+	
+	
+	
+	
+}
