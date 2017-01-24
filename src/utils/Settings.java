f340a75e16ee2aafe55bfc87f7054959734a8a39
Bartosz Dybała <dybala.bartosz@gmail.com>
2017-01-24 16:20:22 +0100

    add Settings class

---
+++ src/utils/Settings.java
@@ -0,0 +1,11 @@
+package utils;
+
+public final class Settings {
+
+	// main window
+	final static Double mainWindowWidth = 640.;
+	final static Double mainWindowHeight = 480.;
+	// create new event window
+	final static Double eventFormWidth = 450.;
+	final static Double eventFormHeight = 450.;
+}
